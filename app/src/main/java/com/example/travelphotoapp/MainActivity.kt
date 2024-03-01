5bb91be5a78c79e88dd28905f3f3960931f7bf7a
Adão Gaspar Bartolomeu <adaobartolomeua.b96@hotmail.com>
2024-03-01 19:01:20 +0100

    Bug fixes

--- app/src/main/java/com/example/travelphotoapp/MainActivity.kt
+++ app/src/main/java/com/example/travelphotoapp/MainActivity.kt
@@ -15,32 +15,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val  next = findViewById<ImageButton>(R.id.btn_prev)
+        val  next = findViewById<ImageButton>(R.id.btn_next)
         val  prev = findViewById<ImageButton>(R.id.btn_prev)
 
         next.setOnClickListener{
-            var idCurrentImageString = "pic"+currentImage
+            var idCurrentImageString = "pic$currentImage"
+            
             var idCurrentImageInt = this.resources.getIdentifier(idCurrentImageString,"id",packageName)
             image = findViewById(idCurrentImageInt)
             image.alpha =0f
 
             currentImage= (5+currentImage+1)%5
 
-            var idImageToShowString = "pic"+currentImage
+            var idImageToShowString = "pic$currentImage"
+
+            println("NEXT "+idImageToShowString)
             var idImageToShowInt = this.resources.getIdentifier(idImageToShowString,"id",packageName)
             image = findViewById(idImageToShowInt)
             image.alpha =1f
         }
 
         prev.setOnClickListener{
-            var idCurrentImageString = "pic"+currentImage
+            var idCurrentImageString = "pic$currentImage"
             var idCurrentImageInt = this.resources.getIdentifier(idCurrentImageString,"id",packageName)
             image = findViewById(idCurrentImageInt)
             image.alpha =0f
 
             currentImage= (5+currentImage-1)%5
 
-            var idImageToShowString = "pic"+currentImage
+
+            var idImageToShowString = "pic$currentImage"
+            println("PREV "+idImageToShowString)
             var idImageToShowInt = this.resources.getIdentifier(idImageToShowString,"id",packageName)
             image = findViewById(idImageToShowInt)
             image.alpha =1f
