25258f69b550799a4d6a8efa7011570247fdff1d
Adão Gaspar Bartolomeu <adaobartolomeua.b96@hotmail.com>
2024-03-01 17:24:15 +0100

    logic of next and prev button

--- app/src/main/java/com/example/travelphotoapp/MainActivity.kt
+++ app/src/main/java/com/example/travelphotoapp/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.travelphotoapp
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    var  currentImage = 0
+    lateinit var image : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val  next = findViewById<ImageButton>(R.id.btn_prev)
+        val  prev = findViewById<ImageButton>(R.id.btn_prev)
+
+        next.setOnClickListener{
+            var idCurrentImageString = "pic"+currentImage
+            var idCurrentImageInt = this.resources.getIdentifier(idCurrentImageString,"id",packageName)
+            image = findViewById(idCurrentImageInt)
+            image.alpha =0f
+
+            currentImage= (5+currentImage+1)%5
+
+            var idImageToShowString = "pic"+currentImage
+            var idImageToShowInt = this.resources.getIdentifier(idImageToShowString,"id",packageName)
+            image = findViewById(idImageToShowInt)
+            image.alpha =1f
+        }
+
+        prev.setOnClickListener{
+            var idCurrentImageString = "pic"+currentImage
+            var idCurrentImageInt = this.resources.getIdentifier(idCurrentImageString,"id",packageName)
+            image = findViewById(idCurrentImageInt)
+            image.alpha =0f
+
+            currentImage= (5+currentImage-1)%5
+
+            var idImageToShowString = "pic"+currentImage
+            var idImageToShowInt = this.resources.getIdentifier(idImageToShowString,"id",packageName)
+            image = findViewById(idImageToShowInt)
+            image.alpha =1f
+        }
     }
 }
